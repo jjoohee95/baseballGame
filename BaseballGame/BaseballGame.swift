9ab4812b91e114b694041215452f8b420a2da94f
new git <new git>
2024-06-13 21:37:52 +0900

    lv1~2 완료

---
+++ BaseballGame/BaseballGame.swift
@@ -0,0 +1,8 @@
+//
+//  BaseballGame.swift
+//  BaseballGame
+//
+//  Created by t2023-m0023 on 6/13/24.
+//
+
+import Foundation
---
+++ BaseballGame/isValidNumber.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  BaseballGame
+//
+//  Created by t2023-m0023 on 6/13/24.
+//
+
+import Foundation
